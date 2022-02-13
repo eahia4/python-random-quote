f493e42ff1c237341a1cb893c194d5adfb120308
eahia4 <eahiafor01@mynbcc.ca>
2022-02-12 21:45:30 -0400

    Added new lines to print more than one line

--- get-quote.py
+++ get-quote.py
@@ -6,9 +6,11 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+ #last = 13
+ #rnd = random.randint(0, last)
+ # print(random.sample(quotes, 2))
+  sampling = random.sample(quotes, 2)
+  for sample in sampling: print(sample)
 
 if __name__== "__main__":
   primary()
